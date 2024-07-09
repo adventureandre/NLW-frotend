4726b84df8ff94e6fb401e9afc5270dd4a348633
Andre luiz <adventureandre@hotmail.com>
2024-07-08 23:50:25 -0300

    feat: create input

--- src/app.tsx
+++ src/app.tsx
@@ -1,16 +1,27 @@
+import { ArrowRight, Calendar, MapPin } from "lucide-react";
 
 
 export function App() {
 
   return (
     <div className="h-screen flex items-center justify-center ">
-      <div className="max-w-3xl px-6 text-center">
+      <div className="max-w-3xl w-full px-6 text-center space-y-10">
         <p className="text-zinc-300 text-lg ">Convide seus amigos e planeje sua próxima viagem!</p>
 
-        <div >
-          <input type="text "  placeholder="Para onde voçê vai?" />
-          <input type="text "  placeholder="Quando?" />
-          <button>Continuar</button>
+        <div className="h-16 bg-zinc-900 px-4 rounded-xl flex items-center shado " >
+          <div className="flex item-center gap-2 flex-1">
+            <MapPin className="size-5 text-zinc-400" />
+            <input type="text " placeholder="Para onde voçê vai?" className="bg-transparent text-lg placeholder-zinc-400" />
+          </div>
+          <div className="flex item-center gap-2">
+            <Calendar className="size-5 text-zinc-400" />
+            <input type="text " placeholder="Quando?" className="bg-transparent text-lg placeholder-zinc-400" />
+          </div>
+
+          <button className="bg-lime-300 text-lime-950 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-lime-400" >
+            Continuar <ArrowRight className="size-5"/>
+          </button>
+
 
         </div>
 
